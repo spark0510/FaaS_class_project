f8cc9caa0b6685bcc4dde3c46ca7ab3299c82c43
Sungjae Park <123699102+spark0510@users.noreply.github.com>
2023-04-14 01:06:24 -0400

    Create IO_b1.R

---
+++ R/IO_b1.R
@@ -0,0 +1,18 @@
+#!/usr/local/bin/Rscript
+
+a_start_time<-Sys.time()
+start_time <- format(a_start_time, "%Y-%m-%d %H:%M:%OS6")
+
+install.packages("tidyr")
+install.packages("jsonlite")
+install.packages("aws.s3")
+install.packages("RCurl")
+install.packages("dplyr")
+install.packages("httr")
+
+
+a <- system("dd if=/dev/zero of=/tmp/test1.img bs=1 count=500 oflag=dsync", intern=TRUE)
+
+a_end_time<-Sys.time()
+end_time <- format(a_end_time, "%Y-%m-%d %H:%M:%OS6")
+cat('{\"time\":\"',end_time,start_time,'\"}')
